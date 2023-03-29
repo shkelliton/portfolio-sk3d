5df7fe294941b0e53972fa8a91c0f02d9eb93a0f
shkelliton <71197165+shkelliton@users.noreply.github.com>
2023-03-29 01:03:00 -0400

    Reversed index.ts file directory change

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,5 @@
 import "../style.css";
-import typescriptLogo from "./assets/typescript.svg";
+import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter";
 
