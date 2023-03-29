e3d8fc1b1eedbc410a5712cb133a38961f26bbb8
shkelliton <71197165+shkelliton@users.noreply.github.com>
2023-03-29 01:22:13 -0400

    Removed GalleryHome for now

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Navbar from "./NavBar";
 import { Footer } from "./Footer";
@@ -8,7 +7,7 @@ const Layout = () => {
     <>
       <Navbar />
 
-        <Outlet />
+      <Outlet />
 
       <Footer />
     </>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function Navbar() {
